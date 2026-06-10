a9779d4cf3df706bbb1f4890bbb4b9439e53aae0
luvpame <ayuu.kumakuma@gmail.com>
2026-06-10 10:33:01 +0900

    fix: SwiftPM の Nix ビルド警告を抑える

--- flake.nix
+++ flake.nix
@@ -6,10 +6,19 @@
   outputs = { self, nixpkgs }:
     let
       systems = [ "aarch64-darwin" "x86_64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {
+        localSystem = {
+          inherit system;
+          darwinMinVersion = "14.0";
+        };
+      }));
     in
     {
-      packages = forAllSystems (pkgs: rec {
+      packages = forAllSystems (pkgs:
+        let
+          swiftTriple = "${pkgs.stdenv.hostPlatform.darwinArch}-apple-macosx14.0";
+        in
+        rec {
         calpeek = pkgs.stdenv.mkDerivation {
           pname = "calpeek";
           version = "0.1.0";
@@ -17,9 +26,27 @@
 
           nativeBuildInputs = [ pkgs.swift pkgs.swiftpm ];
 
+          NIX_CC_WRAPPER_SUPPRESS_TARGET_WARNING = "1";
+
+          buildPhase = ''
+            runHook preBuild
+            swiftBuild() {
+              swift build \
+                --triple ${swiftTriple} \
+                --cache-path "$TMPDIR/swiftpm-cache" \
+                --config-path "$TMPDIR/swiftpm-config" \
+                --security-path "$TMPDIR/swiftpm-security" \
+                --manifest-cache local \
+                "$@"
+            }
+            swiftBuild -c release
+            swiftBuild -c release --show-bin-path > .swiftpm-bin-path
+            runHook postBuild
+          '';
+
           installPhase = ''
             runHook preInstall
-            install -Dm755 "$(swiftpmBinPath)/calpeek" "$out/bin/calpeek"
+            install -Dm755 "$(cat .swiftpm-bin-path)/calpeek" "$out/bin/calpeek"
             runHook postInstall
           '';
 
